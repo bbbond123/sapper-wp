1a47a7a3140b4e0a9a889d9525ba5ad97b016aa6
yibuwang83 <wangyibu123@gmail.com>
2021-06-07 13:56:31 +0800

    init code

---
+++ src/components/PostList.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+
+  let posts = [];
+
+  const apiURL = process.env.SAPPER_APP_API_URL;
+
+  onMount(async () => {
+    const res = await fetch(`${apiURL}/wp/v2/posts`);
+    const json = await res.json();
+    posts = json;
+    console.log(posts);
+  });
+</script>
+
+<ul>
+  {#each posts as post}
+    <li>{post.title.rendered}</li>
+  {/each}
+</ul>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+
+<svelte:head>
+	<title>Sapper wordpress site</title>
+</svelte:head>
+
+<script>
+	import PostList from '../components/PostList.svelte'
+</script>
+
+
+<PostList /> 
