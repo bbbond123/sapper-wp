ed601885f14492854e8f1e2425ac0fa7c811b28d
yibuwang83 <wangyibu123@gmail.com>
2021-06-07 15:06:24 +0800

    update

--- src/components/PostList.svelte
+++ src/components/PostList.svelte
@@ -17,17 +17,51 @@
     return arr;
   }
 
+  function changePage(page) {
+    fetch(`${apiURL}/wp/v2/posts?per_page=${page}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        posts = result;
+        currentPage = page;
+      });
+  }
+
   onMount(async () => {
     const res = await fetch(`${apiURL}/wp/v2/posts?per_page=5`);
     const json = await res.json();
+
+    totalPages = res.headers.get("X-WP-TotalPages");
+    pages = createPagesArray(totalPages);
+    console.log("🚀 ~ file: PostList.svelte ~ line 26 ~ onMount ~ pages", pages);
+
     posts = json;
   });
 </script>
 
-<ul>
+<ul class="post-list">
   {#each posts as post}
     <li>
       <a rel="prefetch" href={`articles/${post.slug}`}>{post.title.rendered}</a>
     </li>
   {/each}
 </ul>
+<ul class="pagination">
+  {#each pages as page}
+    <li>
+      <button
+        on:click={() => {
+          console.log(page);
+          changePage(page);
+        }}
+      >
+        {page}
+      </button>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  /* your styles go here */
+</style>
