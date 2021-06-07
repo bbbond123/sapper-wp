91330650eed49a7c9f7f20d4b56efe26d20bf0cf
yibuwang83 <wangyibu123@gmail.com>
2021-06-07 14:55:57 +0800

    update code

--- src/components/PostList.svelte
+++ src/components/PostList.svelte
@@ -2,11 +2,23 @@
   import { onMount } from "svelte";
 
   let posts = [];
+  let totalPages = null;
+  let currentPage = 1;
+  let pages = [];
 
   const apiURL = process.env.SAPPER_APP_API_URL;
 
+  function createPagesArray(total) {
+    let arr = [];
+    for (let i = 1; i <= total; i++) {
+      arr.push(i);
+    }
+
+    return arr;
+  }
+
   onMount(async () => {
-    const res = await fetch(`${apiURL}/wp/v2/posts`);
+    const res = await fetch(`${apiURL}/wp/v2/posts?per_page=5`);
     const json = await res.json();
     posts = json;
   });
