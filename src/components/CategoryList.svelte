b95df42e4bda38b53610f549a42dcae8a6fcfa5b
yibuwang83 <wangyibu123@gmail.com>
2021-06-07 16:14:53 +0800

    fix node-sass settings

---
+++ src/components/CategoryList.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  let categories = [];
+
+  const apiUrl = process.env.SAPPER_APP_API_URL;
+
+  onMount(async () => {
+    const res = await fetch(`${apiUrl}/wp/v2/categories?per_page=100`);
+    categories = await res.json();
+  });
+</script>
+
+<ul class="cats post-list">
+  {#each categories as category}
+    <li>
+      <a href={`categories/${category.slug}`}>
+        {category.name}
+      </a>
+    </li>
+  {/each}
+</ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,21 @@
-
-<svelte:head>
-	<title>Sapper wordpress site</title>
-</svelte:head>
-
 <script>
-	import PostList from '../components/PostList.svelte'
+  import PostList from "../components/PostList.svelte";
+  import CategoryList from "../components/CategoryList.svelte";
 </script>
 
+<svelte:head>
+  <title>Sapper wordpress site</title>
+</svelte:head>
 
-<PostList /> 
+<div class="cats-posts">
+  <CategoryList />
+
+  <div class="posts">
+    <PostList />
+  </div>
+</div>
+
+
+<style lang="scss">
+	
+</style>
